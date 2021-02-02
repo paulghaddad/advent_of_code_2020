4f30a58726f2915549f9cfcf3fcd7c47642a7ca3
Paul Haddad <paulh16@gmail.com>
2021-02-02 07:33:41 -0500

    Problem 3B
    * Fix stupid mistake with traversing the Y slope
    * Refactor

--- day3/day3.go
+++ day3/day3.go
@@ -7,7 +7,15 @@ import (
 	"os"
 )
 
-func CountTrees(terrain [][][]byte, xSlope, ySlope int) int {
+// Slope defines the change in the x and y coordinates
+type Slope struct {
+	X int
+	Y int
+}
+
+// CountTrees determines the number of trees encountered on its path from the
+// top-left of the terrain to the bottom
+func CountTrees(terrain [][][]byte, slope Slope) int {
 	treeCount := 0
 	xPos := 0
 	yPos := 0
@@ -20,11 +28,24 @@ func CountTrees(terrain [][][]byte, xSlope, ySlope int) int {
 			treeCount++
 		}
 
-		xPos += xSlope
-		yPos++
+		xPos += slope.X
+		yPos += slope.Y
 	}
 
 	return treeCount
+}
+
+// MultipleRoutes returns the product of the number of trees encountered for
+// for all routes taken
+func MultipleRoutes(terrain [][][]byte, slopes []Slope) int {
+	productOfTrees := 1
+
+	for _, slope := range slopes {
+		treesEncountered := CountTrees(terrain, slope)
+		productOfTrees *= treesEncountered
+	}
+
+	return productOfTrees
 }
 
 func main() {
@@ -48,6 +69,23 @@ func main() {
 		terrain = append(terrain, chars)
 	}
 
-	numTrees := CountTrees(terrain, 3, 1)
-	fmt.Printf("The number of trees encountered was %d\n", numTrees)
+	// Problem A
+
+	numTrees := CountTrees(terrain, Slope{X: 3, Y: 1})
+	fmt.Printf("Problem A: The number of trees encountered was %d\n", numTrees)
+
+	// Problem B
+
+	product := 1
+	slopes := []Slope{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	product = MultipleRoutes(terrain, slopes)
+
+	fmt.Printf("Problem B: The product of the trees encountered is: %d\n", product)
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -21,18 +21,62 @@ func TestCountTrees(t *testing.T) {
 	testTerrain = append(testTerrain, bytes.Split([]byte(".#..#...#.#"), []byte("")))
 
 	var tests = []struct {
-		terrain        [][][]byte
-		xSlope, ySlope int
-		want           int
+		terrain [][][]byte
+		slope   Slope
+		want    int
 	}{
-		{testTerrain, 3, 1, 7},
+		{testTerrain, Slope{X: 3, Y: 1}, 7},
 	}
 
 	for _, tt := range tests {
-		testname := fmt.Sprintf("xSlope %d, ySlope: %d\n", tt.xSlope, tt.ySlope)
+		testname := fmt.Sprintf("Slope: %v\n", tt.slope)
 
 		t.Run(testname, func(t *testing.T) {
-			got := CountTrees(tt.terrain, tt.xSlope, tt.ySlope)
+			got := CountTrees(tt.terrain, tt.slope)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultipleRoutes(t *testing.T) {
+	testTerrain := [][][]byte{}
+	testTerrain = append(testTerrain, bytes.Split([]byte("..##......."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte("#...#...#.."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte(".#....#..#."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte("..#.#...#.#"), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte(".#...##..#."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte("..#.##....."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte(".#.#.#....#"), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte(".#........#"), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte("#.##...#..."), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte("#...##....#"), []byte("")))
+	testTerrain = append(testTerrain, bytes.Split([]byte(".#..#...#.#"), []byte("")))
+
+	var tests = []struct {
+		terrain [][][]byte
+		slopes  []Slope
+		want    int
+	}{
+		{
+			testTerrain,
+			[]Slope{
+				{X: 1, Y: 1},
+				{X: 3, Y: 1},
+				{X: 5, Y: 1},
+				{X: 7, Y: 1},
+				{X: 1, Y: 2},
+			},
+			336,
+		}}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Slope %v\n", tt.slopes)
+
+		t.Run(testname, func(t *testing.T) {
+			got := MultipleRoutes(tt.terrain, tt.slopes)
 
 			if got != tt.want {
 				t.Errorf("got: %v; want: %v", got, tt.want)
