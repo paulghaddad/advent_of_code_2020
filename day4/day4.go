b7db27aae5c84e4815c97935bd96c062860a477a
Paul Haddad <paulh16@gmail.com>
2021-02-15 08:00:02 -0500

    Day 4B

--- day4/day4.go
+++ day4/day4.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -38,6 +40,125 @@ func IsValidPassport(input string) bool {
 	return true
 }
 
+// IsValidPassportStrict validates the individual fields of each passport, in
+// addition to the criteria in IsValidPassport
+func IsValidPassportStrict(input string) bool {
+	if !IsValidPassport(input) {
+		return false
+	}
+
+	passport := make(map[string]string)
+
+	for _, kv := range strings.Split(strings.TrimSpace(input), " ") {
+		pair := strings.Split(kv, ":")
+		passport[pair[0]] = pair[1]
+	}
+
+	parsedByr, _ := strconv.Atoi(passport["byr"])
+	if !ValidBirthyear(parsedByr) {
+		return false
+	}
+
+	parsedIyr, _ := strconv.Atoi(passport["iyr"])
+	if !ValidIssueyear(parsedIyr) {
+		return false
+	}
+
+	parsedEyr, _ := strconv.Atoi(passport["eyr"])
+	if !ValidExpirationyear(parsedEyr) {
+		return false
+	}
+
+	if !ValidHeight(passport["hgt"]) {
+		return false
+	}
+
+	if !ValidHaircolor(passport["hcl"]) {
+		return false
+	}
+
+	if !ValidEyecolor(passport["ecl"]) {
+		return false
+	}
+
+	if !ValidPassportID(passport["pid"]) {
+		return false
+	}
+
+	return true
+}
+
+// ValidBirthyear returns true if: four digits; at least 1920 and at most 2002.
+func ValidBirthyear(year int) bool {
+	return year >= 1920 && year <= 2002
+}
+
+// ValidIssueyear returns true if: four digits; at least 2010 and at most 2020.
+func ValidIssueyear(year int) bool {
+	return year >= 2010 && year <= 2020
+}
+
+// ValidExpirationyear returns true if: four digits; at least 2020 and at most 2030.
+func ValidExpirationyear(year int) bool {
+	return year >= 2020 && year <= 2030
+}
+
+// ValidHeight returns whether a given height in in or cm is within a valid
+// range
+func ValidHeight(height string) bool {
+	r, _ := regexp.Compile("([1-9][0-9]{1,2})(in|cm)")
+	match := r.MatchString(height)
+	if !match {
+		return false
+	}
+
+	groups := r.FindStringSubmatch(height)
+	num, _ := strconv.Atoi(groups[1])
+
+	if groups[2] == "cm" && num >= 150 && num <= 193 {
+		return true
+	}
+
+	if groups[2] == "in" && num >= 59 && num <= 76 {
+		return true
+	}
+
+	return false
+}
+
+// ValidHaircolor returns whether the haircolor code is valid
+func ValidHaircolor(color string) bool {
+	match, _ := regexp.MatchString("#[0-9a-f]{6}", color)
+	return match
+}
+
+// ValidEyecolor returns whether the eye color is valid
+func ValidEyecolor(color string) bool {
+	validEyecolors := [7]string{
+		"amb",
+		"blu",
+		"brn",
+		"gry",
+		"grn",
+		"hzl",
+		"oth",
+	}
+
+	for _, validColor := range validEyecolors {
+		if color == validColor {
+			return true
+		}
+	}
+
+	return false
+}
+
+// ValidPassportID returns whether a PID is valid
+func ValidPassportID(pid string) bool {
+	match, _ := regexp.MatchString("^[0-9]{9}$", pid)
+	return match
+}
+
 func main() {
 	filename := os.Args[1]
 
@@ -50,13 +171,20 @@ func main() {
 
 	var buf bytes.Buffer
 	validPassports := 0
+	validPassportsStrict := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		if strings.Compare(line, "") == 0 {
-			if IsValidPassport(buf.String()) {
+			bufString := buf.String()
+			if IsValidPassport(bufString) {
 				validPassports++
+			}
+
+			if IsValidPassportStrict(bufString) {
+				validPassportsStrict++
+
 			}
 			buf.Truncate(0)
 		}
@@ -65,7 +193,7 @@ func main() {
 		buf.WriteByte(' ')
 	}
 
-	fmt.Printf("There are %d valid passports.\n", validPassports)
+	fmt.Printf("There are %d valid passports and %d valid string passports.\n", validPassports, validPassportsStrict)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading input")
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -28,3 +28,188 @@ func TestIsValidPassport(t *testing.T) {
 		})
 	}
 }
+
+func TestIsValidPassportStrict(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926", false},
+		{"iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946", false},
+		{"hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277", false},
+		{"hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007", false},
+		{"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f", true},
+		{"eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm", true},
+		{"hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022", true},
+		{"iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719", true},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %s\n", tt.input)
+
+		t.Run(testname, func(t *testing.T) {
+			got := IsValidPassportStrict(tt.input)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidBirthyear(t *testing.T) {
+	var tests = []struct {
+		year int
+		want bool
+	}{
+		{2002, true},
+		{2003, false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %d\n", tt.year)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidBirthyear(tt.year)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidIssueyear(t *testing.T) {
+	var tests = []struct {
+		year int
+		want bool
+	}{
+		{2014, true},
+		{2009, false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %d\n", tt.year)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidIssueyear(tt.year)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidExpirationyear(t *testing.T) {
+	var tests = []struct {
+		year int
+		want bool
+	}{
+		{2021, true},
+		{2019, false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %d\n", tt.year)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidExpirationyear(tt.year)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidHeight(t *testing.T) {
+	var tests = []struct {
+		height string
+		want   bool
+	}{
+		{"60in", true},
+		{"190cm", true},
+		{"190in", false},
+		{"190", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %s\n", tt.height)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidHeight(tt.height)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidHaircolor(t *testing.T) {
+	var tests = []struct {
+		haircolor string
+		want      bool
+	}{
+		{"#123abc", true},
+		{"#123abz", false},
+		{"123abc", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %s\n", tt.haircolor)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidHaircolor(tt.haircolor)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidEyecolor(t *testing.T) {
+	var tests = []struct {
+		eyecolor string
+		want     bool
+	}{
+		{"brn", true},
+		{"wat", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %s\n", tt.eyecolor)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidEyecolor(tt.eyecolor)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidPassportID(t *testing.T) {
+	var tests = []struct {
+		pid  string
+		want bool
+	}{
+		{"000000001", true},
+		{"0123456789", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("Input: %s\n", tt.pid)
+
+		t.Run(testname, func(t *testing.T) {
+			got := ValidPassportID(tt.pid)
+
+			if got != tt.want {
+				t.Errorf("got: %v; want: %v", got, tt.want)
+			}
+		})
+	}
+}
